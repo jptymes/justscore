ffcc6c11256cf8de65d6ef2cdc0415f4a2f26b1e
jptymes <john@justintymesports.com>
2026-04-18 10:49:06 -0400

    v4.20 Added HORSE Game

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'justscore-v4.19c';
+const CACHE = 'justscore-v4.20';
 const ASSETS = [
   '/', '/index.html', '/manifest.json', '/sw.js', '/favicon.ico',
   '/icon-72.png', '/icon-96.png', '/icon-120.png', '/icon-152.png',
